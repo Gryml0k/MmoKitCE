6acb1e5db48695f9681a1f1a164ce6b1c027487f
John Money <account@john.money>
2026-03-20 19:17:07 -0400

    move MMO/Scripts/MMOGame/Src to subtree

--- MMO/Scripts/MMOGame/Src/MapSpawn/SpawnAllocateMapByNameData.cs
+++
@@ -1,9 +0,0 @@
-namespace MultiplayerARPG.MMO
-{
-    [System.Serializable]
-    public struct SpawnAllocateMapByNameData
-    {
-        public string mapName;
-        public int allocateAmount;
-    }
-}
--- MMO/Scripts/MMOGame/Src/README.md
+++
@@ -1,2 +0,0 @@
-# UnityMultiplayerARPG_MMOSource
-Central/Database management/Map Spawn/Cluster server's source codes which will be shared between projects 
